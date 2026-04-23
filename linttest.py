d2fc533dfcf824e8a8b3f6dba277753f5b39bbe5
mod-elise <mod@kijko.com>
2026-04-23 14:46:09 +0100

    sillytest

---
+++ linttest.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+
+def generate_password():
+    password = "abc123"
+    unused_variable = 42
+    print( password )
+    return password
+
+
+def another_function( ):
+  x=1+2
+  return x
