90800d1d2e9746033803ee5c431d95a51452f2ce
mod-elise <mod@kijko.com>
2026-04-24 13:42:13 +0100

    separates ui from logic

---
+++ password_generator/main.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+from password_generator.generator import generate_password
+
+
+
+## USER  INTERFACE ##
+
+passwordrow = 0
+sliderrow = 1
+checkboxrow = 2
+
+
+def pressGenerate(pwlength, uppercase, digits, symbols):
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, generate_password(pwlength, uppercase, digits, symbols))
+
+
+def pressCopy():
+    root.clipboard_clear()
+    root.clipboard_append(entry.get())
+    root.update()
+
+
+root = tk.Tk()
+root.title("Password Generator")
+entry = tk.Entry(root, width=34, font=("Arial", 18))
+entry.grid(row=passwordrow, column=0, columnspan=3)
+hasUppercase = tk.BooleanVar()
+hasDigits = tk.BooleanVar()
+hasSymbols = tk.BooleanVar()
+passwordLength = tk.IntVar()
+
+
+slider = tk.Scale(
+    root,
+    from_=4,
+    to=25,
+    label="Password Length",
+    variable=passwordLength,
+    length=400,
+    orient=tk.HORIZONTAL,
+).grid(row=sliderrow, column=0, columnspan=3)
+
+
+upCheck = tk.Checkbutton(root, text="uppercase", variable=hasUppercase).grid(
+    row=checkboxrow, column=0
+)
+digCheck = tk.Checkbutton(root, text="digits", variable=hasDigits).grid(
+    row=checkboxrow, column=1
+)
+specCheck = tk.Checkbutton(root, text="special", variable=hasSymbols).grid(
+    row=checkboxrow, column=2
+)
+
+tk.Button(
+    root,
+    text="Generate",
+    width=10,
+    height=4,
+    bg="LIGHT GREEN",
+    command=lambda: pressGenerate(
+        passwordLength.get(), hasUppercase.get(), hasDigits.get(), hasSymbols.get()
+    ),
+).grid(row=checkboxrow, column=4)
+
+tk.Button(root, text="Copy", width=10, command=lambda: pressCopy()).grid(
+    row=passwordrow, column=4
+)
+
+
+root.mainloop()
