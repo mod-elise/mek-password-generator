f703e1fb191d719cf9c48fbffdef8d04d5a320a2
mod-elise <mod@kijko.com>
2026-04-24 15:01:40 +0100

    improving strength reading

--- password_generator/main.py
+++ password_generator/main.py
@@ -16,6 +16,9 @@ def pressGenerate(pwlength, uppercase, digits, symbols):
         "Strong": "green"
     }
     frame.config(bg=strength_colors[strength])
+    strength_label.config(text=strength, bg=strength_colors[strength])
+
+
 
 
 def pressCopy():
@@ -46,7 +49,9 @@ slider = tk.Scale(
 
 frame = tk.Frame(root, bg="grey", width=50, height=50, bd=3, relief=tk.RIDGE)
 frame.grid(row=sliderrow, column=4)
-
+frame.pack_propagate(False)
+strength_label = tk.Label(frame, text="", bg="grey")
+strength_label.pack()
 
 upCheck = tk.Checkbutton(root, text="uppercase", variable=hasUppercase).grid(
     row=checkboxrow, column=0
